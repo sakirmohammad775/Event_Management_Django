5e2006b166bc223beb321393ec5d9ee5a09c48b0
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-08-20 20:53:50 +0600

    requirements text add

--- event_management/settings.py
+++ event_management/settings.py
@@ -11,7 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ----------------------------
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS=['HTTPS://*.onrender.com','http://127.0.0.1:8000']
 
 # ----------------------------
 # Installed Apps
